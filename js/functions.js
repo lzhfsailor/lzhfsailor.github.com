098f228e46ac5e83a7dbf3cb65d4e1c6f916933a
Himin·Liang <lzfsailor@163.com>
2014-01-04 16:15:54 +0800

    day

--- js/functions.js
+++ js/functions.js
@@ -86,7 +86,7 @@ function startHeartAnimation() {
 function timeElapse(c) {
 	var e = Date();
 	var f = (Date.parse(e) - Date.parse(c)) / 1000;
-	var g = Math.floor(f / (3600 * 24))+31;
+	var g = Math.floor(f / (3600 * 24))+32;
 	f = f % (3600 * 24);
 	var b = Math.floor(f / 3600);
 	if (b < 10) {
